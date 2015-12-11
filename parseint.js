0df35c49453b23338a28ddfcd0fd349e1229e653
Michael Kühnel <mail@michael-kuehnel.de>
2015-12-11 17:15:10 +0100

    Bump version number
    No breaking change :tada:

--- parseint.js
+++ parseint.js
@@ -4,7 +4,7 @@
  * @param {boolean} [random=false] sets the return value to a random number between 1 - 9.
  * @return {int}             the parsed value or a random number
  * @author                   bullgit
- * @version                  1.0
+ * @version                  1.1
  */
 var parseint = function(variable, random) {
   var returnValue = random === undefined ? 4 : Math.floor(Math.random() * (9 - 1 + 1)) + 1;
